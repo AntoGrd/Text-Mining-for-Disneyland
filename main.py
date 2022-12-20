58fe054037f1dbdbd761a6a5778e2c678bbf5058
Samibgh <78345903+Samibgh@users.noreply.github.com>
2022-12-21 00:47:47 +0100

    change

--- main.py
+++ main.py
@@ -49,12 +49,22 @@ for i in range(0,len(url_parc)):
 #os.chdir(r"C:\Documents\travail\LYON2\M2\text_mining\projet_disney\projet_disney\data")
 #tab=pd.read_csv("hotel_marvel.csv")
 #os.chdir(r"C:\Documents\travail\LYON2\M2\text_mining\projet_disney\projet_disney")
-os.chdir("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data")
-tab=pd.read_csv("Walt_Disney_Studios_Park.csv")
-os.chdir(r"C:\Users\Sam\Documents\GitHub\Text-Mining-for-Disneyland")
-translate(tab)
-tab = clean_data_parc(tab)
-tab.to_csv('hotel_marvel_clean.csv', index=False, encoding = 'utf-8-sig')
+for i in nom_hotels:
+    os.chdir("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data")
+    tab=pd.read_csv(str(i) + ".csv")
+    os.chdir(r"C:\Users\Sam\Documents\GitHub\Text-Mining-for-Disneyland")
+    translate(tab)
+    tab = clean_data_hotel(tab)
+    tab.to_csv(str(i) + "_clean.csv", index=False, encoding = 'utf-8-sig')
+
+
+for i in namesParc:
+    os.chdir("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data")
+    tab=pd.read_csv(str(i) + ".csv")
+    os.chdir(r"C:\Users\Sam\Documents\GitHub\Text-Mining-for-Disneyland")
+    translate(tab)
+    tab = clean_data_parc(tab)
+    tab.to_csv(str(i) + "_clean.csv", index=False, encoding = 'utf-8-sig')
 
 
 #<<<<<<< HEAD
