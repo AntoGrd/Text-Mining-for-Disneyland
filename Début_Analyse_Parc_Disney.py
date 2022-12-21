e46a7ee460cfd93c0b2acfc24f43db13894957d1
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2022-12-21 14:58:11 +0100

    Update Début_Analyse_Parc_Disney.py

--- Début_Analyse_Parc_Disney.py
+++ Début_Analyse_Parc_Disney.py
@@ -12,13 +12,12 @@ modalites = pd.unique(parcDisney['Note'])
 print(parcDisney.Note.value_counts())
 
 parseur = CountVectorizer()
-
 # Fonction qui donne les 5 (par défault) mots les plus cités dans le dataframe (df) pour toutes les notes (par défault) 
 def mots_significatif_par_note(df,note='tous',nb_mots = 5):
     # Récupération du dataframe pour toutes les notes)
     if (note == 'tous') :
         dfnew = df
-    # récupération du dataframe si l'on ne prend pas toutes les notes
+    # Récupération du dataframe si l'on ne prend pas toutes les notes
     else:
         a = df.columns
         dfnew =  pd.DataFrame(columns=a)
@@ -28,16 +27,16 @@ def mots_significatif_par_note(df,note='tous',nb_mots = 5):
     X = parseur.fit_transform(dfnew['commentaire'])
     mdt = X.toarray()
     freq_mots = np.sum(mdt,axis=0)
+    # Récupération des index des mots qui reviennent le plus (ordre décroissant)
     index = np.argsort(freq_mots)
-    for i in range(freq_mots.shape[0]):
-        index = index[::-1]
-    #affichage des 5 mots qui ressortent le plus (ou un autre nombre si l'on a mit autre chose que 5)
-    print("Nombre de mots distinct dans notre dataframe : "+ str(i))
-    for j in range(nb_mots):
-        print(np.asarray(parseur.get_feature_names())[index[j]],freq_mots[index[j]])
+    # On met les index dans l'ordre croissant
+    index = np.flip(index)
+    # Affichage des 5 mots qui ressortent le plus (ou un autre nombre si l'on a mit autre chose que 5)
+    print("Nombre de mots distinct dans notre dataframe : "+ str(freq_mots.shape[0]))
+    for k in range(nb_mots):
+        print(np.asarray(parseur.get_feature_names())[index[k]],freq_mots[index[k]])
     print("")
 
 # Exemple d'utilisation 
-mots_significatif_par_note(parcDisney)
-mots_significatif_par_note(parcDisney,[1,2,3],8)
-
+mots_significatif_par_note(df = parcDisney)
+mots_significatif_par_note(df = parcDisney,note = [2,3,4], nb_mots = 8)
