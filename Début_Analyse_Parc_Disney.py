4f9cf1adfe1ccd0a2d408b9d00c8368cbc82a0ef
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2022-12-21 19:03:01 +0100

    Update Début_Analyse_Parc_Disney.py

--- Début_Analyse_Parc_Disney.py
+++ Début_Analyse_Parc_Disney.py
@@ -7,36 +7,49 @@ parcDisney = pd.read_csv("C:/Users/laura/Downloads/Text-Mining-for-Disneyland-ma
 parcDisney.info( ) #20685 avis
 
 parcDisney.head() 
-parcDisney['Date'].tail(1)
+parcDisney['Date_Avis'].tail(1)
 modalites = pd.unique(parcDisney['Note'])
 print(parcDisney.Note.value_counts())
 
+parseur = CountVectorizer()
+
 # Fonction qui donne les 5 (par défault) mots les plus cités dans le dataframe (df) pour toutes les notes (par défault) 
-def mots_significatif_par_note(df,note='tous',nb_mots = 5):
+def mots_significatif_par_note(df, variable = 'toutes', modalité = 'toutes',  nb_mots = 5):
     # Récupération du dataframe pour toutes les notes)
-    if (note == 'tous') :
-        dfnew = df
-    # Récupération du dataframe si l'on ne prend pas toutes les notes
-    else:
-        a = df.columns
-        dfnew =  pd.DataFrame(columns=a)
-        for i in note:
-            print(i)
-            dfnew = dfnew.append(df[df['Note']==i])
+    a = df.columns
+    dfnew = pd.DataFrame(columns=a)
+    # Si base complète on prend tout notre dataframe
+    if (variable == 'toutes') :
+        if (modalité == 'toutes'):
+            dfnew = df
+    # Si on choisie que quelques variables 
+    else :
+        # Si on prend toutes les modalités on prend tout notre dataFrame
+        for i in range (0,len(variable)) : 
+            if (modalité[i] == 'toutes'):
+                dfnew = df
+            else :
+                # Sinon on récupère uniquement les lignes que l'on souhate
+                for j in range (0,len(modalité[i])):
+                    # On récupère l'ensemble des modalités de notre variable
+                    dfnew = dfnew.append(df[df[variable[i]]==modalité[i][j]])
     parseur = CountVectorizer()
     X = parseur.fit_transform(dfnew['commentaire'])
     mdt = X.toarray()
+    # On compte la fréquence de chaque mots dans notre DataFrame
     freq_mots = np.sum(mdt,axis=0)
     # Récupération des index des mots qui reviennent le plus (ordre décroissant)
     index = np.argsort(freq_mots)
     # On met les index dans l'ordre croissant
     index = np.flip(index)
+    # On affiche les variables et les modalités de notre DataFrame et les nombre de mot total dans ce dernier
+    print("Nombre de mots distinct dans notre dataframe contenant les variables : " + str(variable) ,"et les modalités " + str(modalité) ,": "+ str(freq_mots.shape[0]))
     # Affichage des 5 mots qui ressortent le plus (ou un autre nombre si l'on a mit autre chose que 5)
-    print("Nombre de mots distinct dans notre dataframe : "+ str(freq_mots.shape[0]))
     for k in range(nb_mots):
         print(np.asarray(parseur.get_feature_names())[index[k]],freq_mots[index[k]])
     print("")
 
 # Exemple d'utilisation 
 mots_significatif_par_note(df = parcDisney)
-mots_significatif_par_note(df = parcDisney,note = [2,3,4], nb_mots = 8)
+mots_significatif_par_note(df = parcDisney, variable = ['Note','Pays'], modalité = [[4,5],[' France',' Espagne']], nb_mots = 8)
+
