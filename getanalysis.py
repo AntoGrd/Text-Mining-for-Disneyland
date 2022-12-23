59fa080fcff837ae2a5470379005aacd8c3458e3
Chrys_Grosso <114347666+Xhrys69@users.noreply.github.com>
2022-12-23 15:50:20 +0100

    Create getanalysis.py
    analyse_sentiments

---
+++ getanalysis.py
@@ -0,0 +1,8 @@
+#lancement de l'analyse des sentiments
+def getAnalysis(score):
+    if score < 0:
+        return 'Negative'
+    elif score == 0:
+        return 'Neutral'
+    else:
+        return 'Positive'
