b596f116f98f19266b6f5367e529a7f88d398aea
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-02 17:26:18 +0100

    Update représentation_graphique_mots.py

--- représentation_graphique_mots.py
+++ représentation_graphique_mots.py
@@ -1,6 +1,6 @@
 # IMPORTATION
 # /!\ Il faut récupérer la fonction GetAnalysis 
-# On peut aussi par la suite appler des fonction du fichier ... pour plus d'infos 
+# On peut aussi par la suite appler des fonction du fichier Similarité_de_mots.py pour plus d'infos 
 import wordcloud
 import seaborn as sns
 import matplotlib.pyplot as plt
