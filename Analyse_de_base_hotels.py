c75284a6a417bb7995ede468c435fc1d24407123
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-07 00:19:50 +0100

    Update Analyse_de_base_hotels.py

--- Analyse_de_base_hotels.py
+++ Analyse_de_base_hotels.py
@@ -1,35 +1,25 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 
-parcDisney = pd.read_csv("/content/Disneyland_Paris_clean.csv", sep=",")
-parcDisney.info( )
-
-studio = pd.read_csv("/content/Walt_Disney_Studios_Park_clean.csv", sep = ',')
-studio.info()
-
 def nombre_avis_par_années(df):
-  print(df.Annee_Avis.value_counts())
- 
-nombre_avis_par_années(parcDisney)
-nombre_avis_par_années(studio)
+  df.Annee_Avis.value_counts()
+  st.write
 
 def répartition_des_notes(df):
-  plt.figure(figsize=(6, 6))
   cols = ['b','c','y','orange','r']
-  df['Note'].value_counts().plot.pie(autopct='%1.1f%%', shadow= True,colors=cols)
-  plt.show()
-
-répartition_des_notes(studio)
-répartition_des_notes(parcDisney)
+  val = df['Note'].value_counts()
+  val.plot.pie(autopct='%1.1f%%', shadow= True,colors=cols)
+  st.pyplot
 
 def notes(df):
   comptage=df['langue'].value_counts()
   comptage.plot(kind='bar',stacked=True,ylabel="Nombre de répondants",xlabel="Langue de l'avis")
+  st.pyplot
+
+def notes1(df):
   T=pd.crosstab(df['langue']=='fr',df['Note'], normalize='index')
   T.plot.barh()
-  plt.show()
-
-notes(parcDisney)
-notes(studio)
-
+  st.pyplot
