18e4a2f0df13bd49dccc33eca3e8497294fcd71b
Samibgh <78345903+Samibgh@users.noreply.github.com>
2023-01-10 22:29:19 +0100

    update

--- fonctions_analyse.py
+++ fonctions_analyse.py
@@ -30,7 +30,7 @@ from tqdm import tqdm
 tqdm.pandas()
 from geopy.geocoders import Nominatim
 from gensim.models import Word2Vec
-
+from cleanData import clean_data_hotel,clean_commentaire
 
 #Transform list 
 def flatten(lis):
@@ -302,3 +302,80 @@ def get_continent(lat: float, lon:float) -> Tuple[str, str]:
     
     return country, continent_name, lat, lon, city 
 
+
+
+def applyCountry(i):
+    
+
+        latitude = []
+        longitude = []
+        list_continent = []
+        list_pays = []
+        list_lat = []
+        list_lon = []
+        list_city = []
+        
+        geolocator  = Nominatim(user_agent = "geoapiExercises")
+
+        
+        os.chdir("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data_translate")
+        tab=pd.read_csv(str(i) + "_fr.csv")
+        tab = clean_data_hotel(tab)
+        tab = clean_commentaire(tab)
+        
+        #d_sentiment = add_Sentiment(tab) #ajouter la colonne sentiment sur les commentaires
+        
+        #ajouter la lattitude et longitudes des villes 
+        for ville in tab['Ville'] : 
+        
+            try :
+                location = geolocator.geocode(str(ville))
+                latitude.append(location.latitude)
+                longitude.append(location.longitude)
+        
+            except :
+            
+                latitude.append("None")
+        
+                longitude.append("None")
+        
+        tab['latitude_city'] = latitude
+    
+        tab['longitude_city'] = longitude
+    
+        latitude_city = tab['latitude_city'].tolist()
+        longitude_city = tab['longitude_city'].tolist()
+    
+        for z in range(tab.shape[0]) : 
+        
+            try : 
+                continent = get_continent(latitude_city[z], longitude_city[z])
+        
+                list_pays.append(continent[0])
+                list_continent.append(continent[1]) 
+                list_lat.append(continent[2])
+                list_lon.append(continent[3])
+                list_city.append(continent[4])
+            except : 
+                list_pays.append("None")
+                list_continent.append("None")
+                list_lat.append("None")
+                list_lon.append("None")
+                list_city.append("None")
+    
+        tab["Pays_recod"] = list_pays
+        tab["Contient_recod"] = list_continent
+        tab["lat_Pays"] = list_lat
+        tab["Lon_Pays"] = list_lon
+        tab["Ville_recod"] = list_city
+    
+        
+        latitude = []
+        longitude = []
+        list_continent = []
+        list_pays = []
+        list_lat = []
+        list_lon = []
+        list_city = []
+
+        return tab
--- main_analyse .py
+++ main_analyse .py
@@ -5,107 +5,29 @@ Created on Wed Jan  4 17:25:42 2023
 @author: Sam
 """
 
-from fonctions_analyse import get_continent, add_Sentiment
+from fonctions_analyse import get_continent, add_Sentiment, applyCountry
 import pandas as pd
-import os
 from geopy.geocoders import Nominatim
 from tqdm import tqdm
 tqdm.pandas()
-from joblib import Parallel, delayed
-import joblib
-from cleanData import clean_data_hotel, clean_data_parc,clean_commentaire
+from dask import delayed      
 
 
 geolocator  = Nominatim(user_agent = "geoapiExercises")
 
 nom_sites =  ["hotel_marvel","hotel_newport","hotel_sequoia","hotel_cheyenne", "hotel_sante_fe","hotel_davy_crockett"]
         
-import dask
-from dask import delayed      
 
 for i in nom_sites:
     
-    y = delayed(applyCountry)(str(i))
+    y = delayed(applyCountry)("hotel_sequoia")
     tab = pd.DataFrame(y.compute())
     
-    tab.to_csv("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data_translate/" + str(i) + "_ope.csv")
+    tab.to_csv("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data_translate/" + "hotel_newport_ope.csv")
     
     
-        
-def applyCountry(i):
-    
-
-        latitude = []
-        longitude = []
-        list_continent = []
-        list_pays = []
-        list_lat = []
-        list_lon = []
-        list_city = []
         
 
-        
-        os.chdir("C:/Users/Sam/Documents/GitHub/Text-Mining-for-Disneyland/data_translate")
-        tab=pd.read_csv(str(i) + "_fr.csv")
-        tab = clean_data_hotel(tab)
-        tab = clean_commentaire(tab)
-        
-        #d_sentiment = add_Sentiment(tab) #ajouter la colonne sentiment sur les commentaires
-        
-        #ajouter la lattitude et longitudes des villes 
-        for ville in tab['Ville'] : 
-        
-            try :
-                location = geolocator.geocode(str(ville))
-                latitude.append(location.latitude)
-                longitude.append(location.longitude)
-        
-            except :
-            
-                latitude.append("None")
-        
-                longitude.append("None")
-        
-        tab['latitude_city'] = latitude
-    
-        tab['longitude_city'] = longitude
-    
-        latitude_city = tab['latitude_city'].tolist()
-        longitude_city = tab['longitude_city'].tolist()
-    
-        for z in range(tab.shape[0]) : 
-        
-            try : 
-                continent = get_continent(latitude_city[z], longitude_city[z])
-        
-                list_pays.append(continent[0])
-                list_continent.append(continent[1]) 
-                list_lat.append(continent[2])
-                list_lon.append(continent[3])
-                list_city.append(continent[4])
-            except : 
-                list_pays.append("None")
-                list_continent.append("None")
-                list_lat.append("None")
-                list_lon.append("None")
-                list_city.append("None")
-    
-        tab["Pays_recod"] = list_pays
-        tab["Contient_recod"] = list_continent
-        tab["lat_Pays"] = list_lat
-        tab["Lon_Pays"] = list_lon
-        tab["Ville_recod"] = list_city
-    
-        
-        latitude = []
-        longitude = []
-        list_continent = []
-        list_pays = []
-        list_lat = []
-        list_lon = []
-        list_city = []
-
-        return tab
 
 
 
