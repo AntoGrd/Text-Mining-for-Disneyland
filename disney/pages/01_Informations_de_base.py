aa478b15c9b42ff6dce021cad07bc8e8266bf10e
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-10 19:42:09 +0100

    Update 01_Informations_de_base.py

--- disney/pages/01_Informations_de_base.py
+++ disney/pages/01_Informations_de_base.py
@@ -1,33 +1,15 @@
 import streamlit as st
 import pandas as pd
-from Analyse_de_base_hotels import répartition_des_notes,notes,notes1
+from Analyse_de_base_hotels import nombre_avis_par_années,répartition_des_notes,notes,notes1
 
-liste = ['ParcDisney','Studio']
-res = st.sidebar.multiselect("Sectionne un (des) parc(s) et/ou un (des) hotel(s)) ",liste, ('ParcDisney','Studio'))
-df = pd.DataFrame()
-for i in res:
-    if i == 'ParcDisney':
-        df = df.append(pd.read_csv("C:/Users/laura/Downloads/Text-Mining-for-Disneyland-main (1)/Text-Mining-for-Disneyland-main/data_clean/Disneyland_Paris_clean.csv", sep=","))
-    if i == 'Studio':
-        df = df.append(pd.read_csv("C:/Users/laura/Downloads/Text-Mining-for-Disneyland-main (1)/Text-Mining-for-Disneyland-main/data_clean/Walt_Disney_Studios_Park_clean.csv", sep=","))
-
-liste = ['1','2','3','4','5']
-res = st.sidebar.multiselect('Sectionner la ou les notes souhaité',liste, ('1','2','3','4','5'))
-sol = []
-for i in liste:
-  if i not in res :
-    i = int(i)
-    sol.append(i)
-for i in sol:
-    df.drop(df[df['Note'] == i].index,inplace=True)
-print(df['Note'])
 
 st.subheader('Donnée du lieux choisi')
-st.write(df)
 
+st.subheader("Nombre d'avis par année")
+st.bar_chart(nombre_avis_par_années(st.session_state['data']))
 st.subheader('Répartition des notes attribué par le lieux choisie')
-st.pyplot(répartition_des_notes(df))
+st.pyplot(répartition_des_notes(st.session_state['data']))
 st.subheader('Répartition des notes attribué par le lieux choisie')
-st.pyplot(notes(df))
+st.pyplot(notes(st.session_state['data']))
 st.subheader('Répartition des notes attribué par le lieux choisie')
-st.pyplot(notes1(df))
+st.pyplot(notes1(st.session_state['data']))
