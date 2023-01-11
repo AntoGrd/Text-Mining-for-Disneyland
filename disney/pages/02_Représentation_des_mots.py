bd82bca072818a0a983ff8fad5e4d4aa85b56017
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-11 17:07:48 +0100

    Update 02_Représentation_des_mots.py

--- disney/pages/02_Représentation_des_mots.py
+++ disney/pages/02_Représentation_des_mots.py
@@ -1,3 +1,13 @@
 import streamlit as st
+from X_mots import mots_significatif_par_notes2
 
-st.title("Bonjour")
+nb_mots = st.slider('Combien voulez-vous afficher de mots? ', 0, 20, 5)
+st.write('Nomnre de mots choisi', nb_mots)
+
+st.write('Le nombre de mots convient-il ?')
+button = st.button('Oui')
+if button : 
+  st.session_state['nb_mots'] = nb_mots
+  
+st.title("Répartition des mots")
+st.pyplot(mots_sigificatif_par_note2(st.session_state['data'],st.session_state['nb_mots']))
