ad35c0dca693866a7f31f9b950512edf6b189ba9
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-16 16:15:07 +0100

    Update 02_Représentation_des_mots.py

--- pages/02_Représentation_des_mots.py
+++ pages/02_Représentation_des_mots.py
@@ -1,13 +1,19 @@
 import streamlit as st
-from X_mots import mots_significatif_par_notes2
+from X_mots import mots_significatif_par_note2
+from fonctions_analyse import representation_mots
 
-nb_mots = st.slider('Combien voulez-vous afficher de mots? ', 0, 20, 5)
-st.write('Nomnre de mots choisi', nb_mots)
-
-st.write('Le nombre de mots convient-il ?')
-button = st.button('Oui')
-if button : 
-  st.session_state['nb_mots'] = nb_mots
-  
 st.title("Répartition des mots")
-st.pyplot(mots_sigificatif_par_note2(st.session_state['data'],st.session_state['nb_mots']))
+
+nb_mots = st.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
+st.write('Nombre de mots choisi', nb_mots)
+
+st.write('Le nombre de mots convient-il?')
+button = st.button('Oui')
+if button:
+    st.session_state['nb_mots'] = nb_mots
+    if st.session_state['monument']  == 'Parcs':
+        st.pyplot(mots_significatif_par_note2(st.session_state['Parcs'],st.session_state['nb_mots']))
+    if st.session_state['monument']  == 'Hotels':
+        st.pyplot(mots_significatif_par_note2(st.session_state['Hotels'],st.session_state['nb_mots']))
+
+st.pyplot(representation_mots(st.session_state["Parcs"], "commentaire"))
