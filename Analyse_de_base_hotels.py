11f15f9f794c99bfdeb427dab37f3636e87b1a15
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-17 09:54:33 +0100

    Update Analyse_de_base_hotels.py

--- Analyse_de_base_hotels.py
+++ Analyse_de_base_hotels.py
@@ -5,21 +5,42 @@ import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 
 def nombre_avis_par_années(df):
-  df.Annee_Avis.value_counts()
-  st.write
+  res = df.Annee_Avis.value_counts()
+  return res
 
 def répartition_des_notes(df):
   cols = ['b','c','y','orange','r']
   val = df['Note'].value_counts()
   val.plot.pie(autopct='%1.1f%%', shadow= True,colors=cols)
   st.pyplot
+  st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def notes(df):
   comptage=df['langue'].value_counts()
   comptage.plot(kind='bar',stacked=True,ylabel="Nombre de répondants",xlabel="Langue de l'avis")
   st.pyplot
+  st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def notes1(df):
   T=pd.crosstab(df['langue']=='fr',df['Note'], normalize='index')
   T.plot.barh()
   st.pyplot
+  st.set_option('deprecation.showPyplotGlobalUse', False)
+
+def photo_ou_non(df):
+  cols = ['b','c','y','orange','r']
+  val = df['Photo'].value_counts()
+  val.plot.pie(autopct='%1.1f%%', shadow= True,colors=cols)
+  st.pyplot
+  st.set_option('deprecation.showPyplotGlobalUse', False)
+
+def situation_famille(df):
+  cols = ['b','c','y','orange','r']
+  val = df['Situation'].value_counts()
+  val.plot.pie(autopct='%1.1f%%', shadow= True,colors=cols)
+  st.pyplot
+  st.set_option('deprecation.showPyplotGlobalUse', False)
+
+def par_pays(df):
+  res = df.Pays.value_counts()
+  return res
