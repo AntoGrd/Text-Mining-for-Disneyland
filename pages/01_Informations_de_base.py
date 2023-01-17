cd1f0854a55970ce79e6eaf8f6f7200c301f2b69
ChrystelleGrosso <114347666+ChrysGrosso@users.noreply.github.com>
2023-01-17 18:45:15 +0100

    Update 01_Informations_de_base.py

--- pages/01_Informations_de_base.py
+++ pages/01_Informations_de_base.py
@@ -1,14 +1,14 @@
 import streamlit as st
 from Analyse_de_base_hotels import nombre_avis_par_années,répartition_des_notes,notes,notes1,photo_ou_non,situation_famille,par_pays
 
-st.subheader('Donnée du lieux choisi')
+st.subheader('Données du lieu choisi')
 
 #################### PARCS ##############
 if st.session_state['monument']  == 'Parcs':
     # Choix des différents graphiques
     Diagramme = st.sidebar.radio(
         "Quel diagramme voulez-vous afficher ?",
-        ("Nombre d'avis par année", "Répartition des notes", 'Différence des notes entre les français et les étrangers','Répartition des commentaires avec ou sans photos','Répartition des différents types de groupe',"Nombre d'avis par pays"))
+        ("Nombre d'avis par année", "Répartition des notes", 'Différence des notes entre les Français et les étrangers','Répartition des commentaires avec ou sans photos','Répartition des différents types de groupe',"Nombre d'avis par pays"))
 
     # Affichage des graphiques
     if Diagramme == "Nombre d'avis par année":
@@ -35,10 +35,10 @@ if st.session_state['monument']  == 'Hotels':
     # Choix des différents graphiques
     Diagramme = st.sidebar.radio(
         "Quel diagramme voulez-vous afficher ?",
-        ("Nombre d'avis par année", "Répartition des notes", 'Différence des notes entre les français et les étrangers','Répartition des commentaires avec ou sans photos',"Nombre d'avis par pays"))
+        ("Nombre d'avis par année", "Répartition des notes", 'Différence des notes entre les Français et les étrangers','Répartition des commentaires avec ou sans photos',"Nombre d'avis par pays"))
 
     # Affichage des graphiques
-    if Diagramme == "Nombre d'avis par années*":
+    if Diagramme == "Nombre d'avis par années":
         st.subheader("Nombre d'avis par année")
         st.bar_chart(nombre_avis_par_années(st.session_state['Hotels']))
     if Diagramme == "Répartition des notes":
