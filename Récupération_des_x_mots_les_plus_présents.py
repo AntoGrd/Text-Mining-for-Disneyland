c5fe67e2bce062f372076e2262868df88e459c38
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-18 23:07:50 +0100

    Update Récupération_des_x_mots_les_plus_présents.py

--- Récupération_des_x_mots_les_plus_présents.py
+++ Récupération_des_x_mots_les_plus_présents.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+from gensim.models import Word2Vec
+import streamlit as st
+import plotly.graph_objects as go
 
 parseur = CountVectorizer()
 ParcDisney = (pd.read_csv("C:/Users/laura/Downloads/Text-Mining-for-Disneyland-main (1)/Text-Mining-for-Disneyland-main/data_clean/Disneyland_Paris_clean.csv", sep=","))
@@ -58,7 +61,22 @@ def mots_significatif_par_note2(df, nb_mots = 5):
     import matplotlib.pyplot as plt
     poids = imp2['freq'].head(nb_mots)
     bars = imp2['terme'].head(nb_mots)
-    plt.bar(bars,poids)
+    fig = go.Figure(data=[go.Bar(x = bars, y = poids)])
+    return fig
+
+
+def x_mots_plus_courants(df, nb_mots = 5):
+    X = parseur.fit_transform(df['commentaire'])
+    mdt = X.toarray()
+    # On compte la fréquence de chaque mots dans notre DataFrame
+    freq_mots = np.sum(mdt,axis=0)
+    # Récupération des index des mots qui reviennent le plus (ordre décroissant)
+    index = np.argsort(freq_mots)
+    imp = {'terme': np.asarray(parseur.get_feature_names_out())[index], 'freq':freq_mots[index]}
+    imp1 = pd.DataFrame.from_dict(imp, orient='columns')
+    imp2 = imp1.sort_values(by = 'freq', ascending = False)
+    top_des_mots = (imp2['terme'].head(nb_mots))
+    return (top_des_mots)
 
 
 #######################################################################################
