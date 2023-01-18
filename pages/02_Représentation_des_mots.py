d2de1ba65d0e30a2584edf2ff56c9feb29574f83
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-18 23:09:22 +0100

    Update 02_Représentation_des_mots.py

--- pages/02_Représentation_des_mots.py
+++ pages/02_Représentation_des_mots.py
@@ -1,19 +1,100 @@
 import streamlit as st
-from Récupération_des_x_mots_les_plus_présents.py import mots_significatif_par_note2
+from Analyse_de_base_hotels import nuage_de_mots
+from X_mots import mots_significatif_par_note2,x_mots_plus_courants
 from fonctions_analyse import representation_mots
+from Idaviz import lda
+from Similarité_de_mots import most_similar_mots,most_similarity_mots,representation_mots
 
 st.title("Répartition des mots")
 
-nb_mots = st.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
-st.write('Nombre de mots choisi', nb_mots)
 
-st.write('Le nombre de mots convient-il?')
-button = st.button('Oui')
-if button:
+# Choix des différents graphiques
+Diagramme = st.sidebar.radio(
+    "Quel diagramme voulez-vous afficher ?",
+    ("Histogramme des mots qui reviennent le plus", "Vecorisation de mots les plus courant",
+    "Nuage de mots","graph antoine","Similarité des mots","Similarité des mots2", "Similarité des mots3"))
+
+if Diagramme == "Histogramme des mots qui reviennent le plus":
+    nb_mots = st.sidebar.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
+    st.sidebar.write('Nombre de mots choisi', nb_mots)
+    st.sidebar.write('Le nombre de mots convient-il?')
+    button = st.sidebar.button('Oui')
     st.session_state['nb_mots'] = nb_mots
-    if st.session_state['monument']  == 'Parcs':
-        st.pyplot(mots_significatif_par_note2(st.session_state['Parcs'],st.session_state['nb_mots']))
-    if st.session_state['monument']  == 'Hotels':
-        st.pyplot(mots_significatif_par_note2(st.session_state['Hotels'],st.session_state['nb_mots']))
+    if button:
+        st.header("Histogramme des mots qui reviennent le plus")
+        if st.session_state['monument']  == 'Parcs':
+            st.plotly_chart(mots_significatif_par_note2(st.session_state['Parcs'],st.session_state['nb_mots']))
+        if st.session_state['monument']  == 'Hotels':
+            st.plotly_chart(mots_significatif_par_note2(st.session_state['Hotels'],st.session_state['nb_mots']))
 
-st.pyplot(representation_mots(st.session_state["Parcs"], "commentaire"))
+if Diagramme == "Vecorisation de mots les plus courant":
+    nb_mots = st.sidebar.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
+    st.sidebar.write('Nombre de mots choisi', nb_mots)
+    st.sidebar.write('Le nombre de mots convient-il?')
+    button = st.sidebar.button('Oui')
+    st.session_state['nb_mots'] = nb_mots
+    if button:
+        st.header("Vecorisation de mots les plus courant")
+        if st.session_state['monument']  == 'Parcs':
+            st.plotly_chart(representation_mots(st.session_state["Parcs"], "commentaire",st.session_state['nb_mots']))
+        if st.session_state['monument']  == 'Hotels':
+            st.plotly_chart(representation_mots(st.session_state["Parcs"], "commentaire",st.session_state['nb_mots']))              
+
+if Diagramme == "Nuage de mots":
+    st.header("Nuage de mots")
+    if st.session_state['monument']  == 'Parcs':
+        st.write(nuage_de_mots(st.session_state["Parcs"]))
+    if st.session_state['monument']  == 'Hotels':
+        st.write(nuage_de_mots(st.session_state["Hotels"]))             
+
+if Diagramme == "graph antoine":
+    if button:
+        st.header("Graph antoine")
+        if st.session_state['monument']  == 'Parcs':
+            st.write(lda(st.session_state["Parcs"]))
+        if st.session_state['monument']  == 'Hotels':
+            st.write(lda(st.session_state["Hotels"]))
+
+if Diagramme == 'Similarité des mots':
+    nb_mots = st.sidebar.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
+    st.sidebar.write('Nombre de mots choisi', nb_mots)
+    st.session_state['nb_mots'] = nb_mots
+    if st.session_state['monument'] == 'Parcs':
+        selection = x_mots_plus_courants(st.session_state['Parcs'],st.session_state['nb_mots'])
+        option = st.selectbox('Selectionner un mots', selection)
+        st.write(most_similar_mots(st.session_state['Parcs'], option))
+    if st.session_state['monument'] == 'Hotels':
+        selection = x_mots_plus_courants(st.session_state['Hotels'],st.session_state['nb_mots'])
+        option = st.selectbox('Selectionner un mots', 'selection')
+        st.write(most_similar_mots(st.session_state['Hotels'], option))
+
+
+if Diagramme == 'Similarité des mots2':
+    nb_mots = st.sidebar.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
+    st.sidebar.write('Nombre de mots choisi', nb_mots)
+    st.session_state['nb_mots'] = nb_mots
+    if st.session_state['monument'] == 'Parcs':
+        selection = x_mots_plus_courants(st.session_state['Parcs'],st.session_state['nb_mots'])
+        selection2 = x_mots_plus_courants(st.session_state['Parcs'],st.session_state['nb_mots'])
+        option = st.selectbox('Selectionner un mots 1 ', selection)
+        option2 = st.selectbox('Selectionner un mots 2 ', selection2)
+        st.write(most_similarity_mots(st.session_state['Parcs'], option, option2))
+    if st.session_state['monument'] == 'Hotels':
+        selection = x_mots_plus_courants(st.session_state['Hotels'],st.session_state['nb_mots'])
+        selection2 = x_mots_plus_courants(st.session_state['Hotels'],st.session_state['nb_mots'])
+        option = st.radio('Selectionner un mots 1', selection)
+        option2 = st.radio('Selectionner un mots 2', selection2)
+        st.write(most_similarity_mots(st.session_state['Hotels'], option, option2))
+
+if Diagramme == 'Similarité des mots3':
+    nb_mots = st.sidebar.slider('Combien voulez-vous afficher de mots? ', 0, 100, 5)
+    st.sidebar.write('Nombre de mots choisi', nb_mots)
+    st.session_state['nb_mots'] = nb_mots
+    if st.session_state['monument'] == 'Parcs':
+        selection = x_mots_plus_courants(st.session_state['Parcs'],st.session_state['nb_mots'])
+        option = st.multiselect('Selectionner un mots 1 ', selection)
+        st.pyplot(representation_mots(st.session_state['Parcs'], option))
+    if st.session_state['monument'] == 'Hotels':
+        selection = x_mots_plus_courants(st.session_state['Hotels'],st.session_state['nb_mots'])
+        option = st.multiselect('Selectionner un mots 1', selection)
+        st.pyplot(representation_mots(st.session_state['Hotels'], option))
