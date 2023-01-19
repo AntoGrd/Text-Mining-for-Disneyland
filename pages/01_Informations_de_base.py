c153dacb98f9eae1d12e014b6b2c66bab984f974
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-19 14:35:00 +0100

    Update 01_Informations_de_base.py

--- pages/01_Informations_de_base.py
+++ pages/01_Informations_de_base.py
@@ -16,19 +16,22 @@ if st.session_state['monument']  == 'Parcs':
         st.bar_chart(nombre_avis_par_années(st.session_state['Parcs']))
     if Diagramme == "Répartition des notes":
         st.subheader('Répartition des notes attribuées selon le lieu choisi')
-        st.pyplot(répartition_des_notes(st.session_state['Parcs']))
+        st.plotly_chart(répartition_des_notes(st.session_state['Parcs']))
+    if Diagramme == "Répartition des langues des commentaires":
+        st.subheader('Répartition des langues des commentaires')
+        st.bar_chart(notes(st.session_state['Parcs']))
     if Diagramme == "Différence des notes entre les Français et les étrangers":
         st.subheader('Différence des notes entre les Français et les étrangers')
-        st.pyplot(notes1(st.session_state['Parcs']))
+        st.plotly_chart(notes1(st.session_state['Parcs']))
     if Diagramme == "Répartition des commentaires avec ou sans photos":
         st.subheader('Répartition des commentaires avec ou sans photos')
-        st.pyplot(photo_ou_non(st.session_state['Parcs']))
+        st.plotly_chart(photo_ou_non(st.session_state['Parcs']))
     if Diagramme == "Répartition des différents types de groupe":
         st.subheader('Répartition des différents types de groupe')
-        st.pyplot(situation_famille(st.session_state['Parcs']))
+        st.plotly_chart(situation_famille(st.session_state['Parcs']))
     if Diagramme == "Nombre d'avis par pays":
         st.subheader("Nombre d'avis par pays")
-        st.bar_chart(par_pays(st.session_state['Parcs'])) 
+        st.bar_chart(par_pays(st.session_state['Parcs']))
 
 ####################### Hotels ###################
 if st.session_state['monument']  == 'Hotels':
@@ -38,18 +41,22 @@ if st.session_state['monument']  == 'Hotels':
         ("Nombre d'avis par année", "Répartition des notes", 'Différence des notes entre les Français et les étrangers','Répartition des commentaires avec ou sans photos',"Nombre d'avis par pays"))
 
     # Affichage des graphiques
-    if Diagramme == "Nombre d'avis par années":
+    if Diagramme == "Nombre d'avis par année":
         st.subheader("Nombre d'avis par année")
         st.bar_chart(nombre_avis_par_années(st.session_state['Hotels']))
     if Diagramme == "Répartition des notes":
         st.subheader('Répartition des notes attribuées selon le lieu choisi')
-        st.pyplot(répartition_des_notes(st.session_state['Hotels']))
+        st.plotly_chart(répartition_des_notes(st.session_state['Hotels']))
     if Diagramme == "Différence des notes entre les Français et les étrangers":
         st.subheader('Différence des notes entre les Français et les étrangers')
-        st.pyplot(notes1(st.session_state['Hotels']))
+        st.plotly_chart(notes1(st.session_state['Hotels']))
+    if Diagramme == "Répartition des langues des commentaires":
+        st.subheader('Répartition des langues des commentaires')
+        st.bar_chart(notes(st.session_state['Hotels']))
     if Diagramme == "Répartition des commentaires avec ou sans photos":
         st.subheader('Répartition des commentaires avec ou sans photos')
-        st.pyplot(photo_ou_non(st.session_state['Hotels']))
+        st.plotly_chart(photo_ou_non(st.session_state['Hotels']))
     if Diagramme == "Nombre d'avis par pays":
         st.subheader("Nombre d'avis par pays")
-        st.bar_chart(par_pays(st.session_state['Hotels'])) 
+        st.bar_chart(par_pays(st.session_state['Hotels']))
+   
