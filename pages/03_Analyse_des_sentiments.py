e89935cf5ea70178fdbc72a6171be428ad6c632c
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-19 14:23:02 +0100

    Update 03_Analyse_des_sentiments.py

--- pages/03_Analyse_des_sentiments.py
+++ pages/03_Analyse_des_sentiments.py
@@ -1,3 +1,19 @@
 import streamlit as st
+from fonctions_analyse import graph_sentiment,add_Sentiment
+from Text_clustering import get_top_keywords
 
 st.title('hgv')
+
+Diagramme = st.sidebar.radio(
+    "Selectionner sur quelles informations l'analyse des sentiments doit-elle être faite ?",
+    ("Les titres", "Les commentaires"))
+
+if Diagramme == "Les titres":
+    col = 'sentiment_titre_commentaire'
+elif Diagramme == "Les commentaires":
+    col = 'sentiment_commentaire'
+if st.session_state['monument'] == 'Hotels':
+    data = st.session_state['Hotels']
+elif st.session_state['monument']  == 'Parcs':
+    data = st.session_state['Parcs']
+st.plotly_chart(graph_sentiment(add_Sentiment(data),col))
