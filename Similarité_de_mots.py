9909e6950adf01390e450ecea594f26b2188d249
Laura-Boutonnet <62285953+Laura-Boutonnet@users.noreply.github.com>
2023-01-19 14:26:53 +0100

    Update Similarité_de_mots.py

--- Similarité_de_mots.py
+++ Similarité_de_mots.py
@@ -24,14 +24,14 @@ def most_similarity_mots(df, mot1, mot2):
     sim = words.similarity(mot1, mot2)
     st.write(f"Similarité entre {mot1} et {mot2} : {sim}")
 
-def representation_mots(df, liste_mots):
+def representation_mots2(df, liste_mots):
     liste = [ast.literal_eval(x) for x in df.commentaire]
     modele = Word2Vec(liste, vector_size=2, window=5)
     words = modele.wv
     df = pd.DataFrame(words.vectors, columns=["V1","V2"], index = words.key_to_index.keys())
     dfMots = df.loc[liste_mots,:]
-    fig = go.Figure(data=[go.Scatter(x=dfMots.V1, y=dfMots.V2, mode='markers', text=dfMots.index)])
+    fig = go.Figure(data=[go.Scatter(x=dfMots.V1, y=dfMots.V2, mode='markers', text=dfMots.index, name="")])
     fig.update_layout(title={"text": "Répresentation des mots"})
     for i in range (dfMots.shape[0]):
-        fig.add_trace(go.Scatter(x=[dfMots.V1[i]], y=[dfMots.V2[i]], mode='text', text=[dfMots.index[i]]))
+        fig.add_trace(go.Scatter(x=[dfMots.V1[i]], y=[dfMots.V2[i]], name=dfMots.index[i], mode='text', text=[dfMots.index[i]]))
     st.plotly_chart(fig)
