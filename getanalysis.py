98ac200e303f8de7db5ff263843b8b0f2bded2ce
AntoGrd <114435962+AntoGrd@users.noreply.github.com>
2023-01-22 00:51:13 +0100

    Delete getanalysis.py

--- getanalysis.py
+++
@@ -1,8 +0,0 @@
-#lancement de l'analyse des sentiments
-def getAnalysis(score):
-    if score < 0:
-        return 'Negative'
-    elif score == 0:
-        return 'Neutral'
-    else:
-        return 'Positive'
